cf886c35f67d8b399551c522f5ceea0061d0bd3b
Olzhas <106257014+Olzheke2003@users.noreply.github.com>
2024-11-04 11:56:04 +0500

    #1

---
+++ internal/app/apiserver/apiserver.go
@@ -0,0 +1,57 @@
+package apiserver
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
+
+type APIserver struct {
+	config *Config
+	logger *logrus.Logger
+	router *mux.Router
+}
+
+func New(config *Config) *APIserver {
+	return &APIserver{
+		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
+	}
+}
+
+func (s *APIserver) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	s.configureRouter()
+
+	s.logger.Info("starting api server")
+	return http.ListenAndServe(s.config.BindAddr, s.router)
+}
+
+func (s *APIserver) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
+	return nil
+}
+
+func (s *APIserver) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIserver) handleHello() http.HandlerFunc {
+	// Локальные перемнные, типы
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Бизнес логика
+		io.WriteString(w, "Hello")
+	}
+}
