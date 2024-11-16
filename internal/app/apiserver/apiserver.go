60ced27267d0f229ae4aa3e2d7849f0480be9484
Your Name <you@example.com>
2024-11-16 07:02:08 +0500

    safafs

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,72 +1,23 @@
 package apiserver
 
 import (
-	"io"
-	"main/internal/app/store"
+	"fmt"
 	"net/http"
-
-	"github.com/gorilla/mux"
-	"github.com/sirupsen/logrus"
 )
 
-type APIserver struct {
-	config *Config
-	logger *logrus.Logger
-	router *mux.Router
-	store  *store.Store
-}
-
-func New(config *Config) *APIserver {
-	return &APIserver{
-		config: config,
-		logger: logrus.New(),
-		router: mux.NewRouter(),
-	}
-}
-
 func (s *APIserver) Start() error {
+	fmt.Println("Server is starting...") // Для отладки
+
 	if err := s.configureLogger(); err != nil {
 		return err
 	}
 
 	s.configureRouter()
 
-	if err := s.configerStore(); err != nil {
-		return err
-	}
-
 	s.logger.Info("starting api server")
-	return http.ListenAndServe(s.config.BindAddr, s.router)
-}
-
-func (s *APIserver) configureLogger() error {
-	level, err := logrus.ParseLevel(s.config.LogLevel)
+	err := http.ListenAndServe(s.config.BindAddr, s.router)
 	if err != nil {
-		return err
+		fmt.Println("Error starting server:", err) // Печать ошибки, если сервер не запускается
 	}
-
-	s.logger.SetLevel(level)
-
-	return nil
-}
-
-func (s *APIserver) configureRouter() {
-	s.router.HandleFunc("/hello", s.handleHello())
-}
-
-func (s *APIserver) configerStore() error {
-	st := store.New(s.config.Store)
-	if err := st.Open(); err != nil {
-		return err
-	}
-	s.store = st
-	return nil
-}
-
-func (s *APIserver) handleHello() http.HandlerFunc {
-	// Локальные переменные, типы (Выполняться всего один раз)
-	return func(w http.ResponseWriter, r *http.Request) {
-		// Бизнес логика
-		io.WriteString(w, "Hello")
-	}
+	return err
 }
---
+++ internal/app/apiserver/server.go
@@ -0,0 +1,37 @@
+package apiserver
+
+import (
+	"main/internal/app/tasks"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
+
+type APIserver struct {
+	config *Config
+	logger *logrus.Logger
+	router *mux.Router
+}
+
+func New(config *Config) *APIserver {
+	return &APIserver{
+		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
+	}
+}
+
+func (s *APIserver) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
+	return nil
+}
+
+func (s *APIserver) configureRouter() {
+	s.router.HandleFunc("/hello", tasks.HandleArchiveInformation)
+}
